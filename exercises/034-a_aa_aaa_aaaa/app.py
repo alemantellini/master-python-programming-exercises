0f444f844dc89ff01cb6ce7f03a3a56e9966548d
Alessandra Mantellini <mantellinialessandra13@gmail.com>
2026-02-11 14:28:22 +0100

    A_aa_aaa_aaaa

--- exercises/034-a_aa_aaa_aaaa/app.py
+++ exercises/034-a_aa_aaa_aaaa/app.py
@@ -1,1 +1,9 @@
+# EJERCICIO 34 - a_aa_aaa_aaaa
 # Your code here
+def computed_value(a):
+    result = 0
+    for i in range(1, 5):
+        new_number = int(str(a)*i)
+        result += new_number
+    return result
+print(computed_value(9))
