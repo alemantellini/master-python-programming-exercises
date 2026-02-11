8fb833e9a365b96cc02948f452eb9655701a313e
Alessandra Mantellini <mantellinialessandra13@gmail.com>
2026-02-11 14:27:23 +0100

    Numbers of letters and digits

--- exercises/032-numbers-of-letters-and-digits/app.py
+++ exercises/032-numbers-of-letters-and-digits/app.py
@@ -1,1 +1,14 @@
+# EJERCICIO 32 - numbers of letters and digits
 # Your code here
+def letters_and_digits(sentence):
+    counts = {
+    "LETTERS": 0,
+    "DIGITS": 0
+}
+    for character in sentence:
+        if character.isalpha():
+            counts["LETTERS"] += 1
+        elif character.isdigit():
+            counts["DIGITS"] += 1
+    return "LETTERS" + " " + str(counts["LETTERS"]) + " " + "DIGITS" + " " + str(counts["DIGITS"])
+print(letters_and_digits("hello world! 123"))
