821fe1dc8241b7892e4cd3329e9d56f43d2f789a
Alessandra Mantellini <mantellinialessandra13@gmail.com>
2026-02-11 14:21:03 +0100

    List and tuple

--- exercises/023-list-and-tuple/app.py
+++ exercises/023-list-and-tuple/app.py
@@ -1,1 +1,11 @@
+# EJERCICIO 23 - list and tuple
 # Your code here
+def list_and_tuple(*args):
+    my_list = []
+    for i in args:
+        my_list.append(str(i))
+    my_tuple = tuple(my_list)
+    print(my_list)
+    print(my_tuple)
+    return my_list, my_tuple
+list_and_tuple(34, 67, 55, 33, 12, 98)
