fb01590bc2373a650805b0ce23952ad8d9477039
Alessandra Mantellini <mantellinialessandra13@gmail.com>
2026-02-11 14:22:26 +0100

    Two-dimensional array

--- exercises/026-two-dimensional-array/app.py
+++ exercises/026-two-dimensional-array/app.py
@@ -1,1 +1,11 @@
+# EJERCICIO 26 - two-dimensional array
 # Your code here
+def two_dimensional_list(x, y):
+    result = []
+    for i in range(x):
+        row = []
+        for j in range(y):
+            row.append(i*j)
+        result.append(row)
+    return result
+print(two_dimensional_list(3, 5))
