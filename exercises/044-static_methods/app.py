79180bb56e16733d1ab3015836acc3526f51c421
Alessandra Mantellini <mantellinialessandra13@gmail.com>
2026-02-11 14:34:37 +0100

    Static methods

--- exercises/044-static_methods/app.py
+++ exercises/044-static_methods/app.py
@@ -1,1 +1,9 @@
+# EJERCICIO 44 - static methods
 # Your code here
+class MathOperations:
+    @staticmethod
+    def add_numbers(num1, num2):
+        return num1 + num2
+operation1 = MathOperations()
+result1 = MathOperations.add_numbers(10, 15)
+print(result1)
