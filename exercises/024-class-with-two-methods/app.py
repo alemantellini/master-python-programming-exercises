df9aa450a0e914b36d38c206e4768871034ed314
Alessandra Mantellini <mantellinialessandra13@gmail.com>
2026-02-11 14:21:28 +0100

    Class with two methods

--- exercises/024-class-with-two-methods/app.py
+++ exercises/024-class-with-two-methods/app.py
@@ -1,1 +1,12 @@
+# EJERCICIO 24 - class with two methods
 # Your code here
+class InputOutString:
+    def __init__(self):
+        self.name = ""
+    def get_string(self):
+        self.name = input("Ingrese su nombre")
+    def print_string(self):
+        print(self.name.upper())
+my_object = InputOutString()
+my_object.get_string()
+my_object.print_string()
