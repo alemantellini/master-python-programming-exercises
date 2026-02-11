fedf2bd341fac5f60d152983b3755225391e21d5
Alessandra Mantellini <mantellinialessandra13@gmail.com>
2026-02-11 14:14:33 +0100

    Car route

--- exercises/015-car_route/app.py
+++ exercises/015-car_route/app.py
@@ -1,7 +1,9 @@
+# EJERCICIO 15 - car route
 # Complete the function to return the amount of days it will take to cover a route
 def car_route(n,m):
-  return None
-
-
+  import math 
+  distance = m / n
+  result = math.ceil(distance)
+  return result
 # Invoke the function with two integers
-print(car_route())
+print(car_route(20, 40))
