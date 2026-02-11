a84e2c312c8eb82fd183c511b3bbbbb8be2856a2
Alessandra Mantellini <mantellinialessandra13@gmail.com>
2026-02-11 14:22:52 +0100

    Sequence of words

--- exercises/027-sequence-of-words/app.py
+++ exercises/027-sequence-of-words/app.py
@@ -1,1 +1,8 @@
+# EJERCICIO 27 - sequence of words
 # Your code here
+def sequence_of_words(text): 
+    my_list = text.split(",")
+    my_list.sort()
+    result = ", ".join(my_list) 
+    return result 
+print(sequence_of_words("without,hello,bag,world"))
