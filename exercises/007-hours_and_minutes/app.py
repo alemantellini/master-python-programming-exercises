79ecbb2b7b6b36128e757fa076aed7d7ce4379e3
Alessandra Mantellini <mantellinialessandra13@gmail.com>
2026-02-11 13:54:38 +0100

    Hours and minutes

--- exercises/007-hours_and_minutes/app.py
+++ exercises/007-hours_and_minutes/app.py
@@ -1,6 +1,9 @@
+# EJERCICIO 7 - hours and minutes
 def hours_minutes(seconds):
   # Your code here
-  return None
-
+  hours = (seconds // 3600)
+  remainder = (seconds % 3600)
+  minutes = (remainder // 60)
+  return hours, minutes
 # Invoke the function and pass any integer as its argument
 print(hours_minutes(3900))
