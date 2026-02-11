c72d7129e757d9e538dce02db94f6e9c9b13a791
Alessandra Mantellini <mantellinialessandra13@gmail.com>
2026-02-11 14:25:47 +0100

    Remove duplicate words

--- exercises/029-remove-duplicate-words/app.py
+++ exercises/029-remove-duplicate-words/app.py
@@ -1,1 +1,9 @@
+# EJERCICIO 29 - remove duplicate words
 # Your code here
+def remove_duplicate_words(text):
+    my_list = text.split()
+    my_set = set(my_list)
+    result = sorted(my_set)
+    x = " ".join(result)
+    return x
+print(remove_duplicate_words("hello world and practice makes perfect and hello world again"))
