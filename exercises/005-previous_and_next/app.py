85f8e770f0f3f7fce90915e3542874be583d6d4d
Alessandra Mantellini <mantellinialessandra13@gmail.com>
2026-02-11 13:52:37 +0100

    Previous and next

--- exercises/005-previous_and_next/app.py
+++ exercises/005-previous_and_next/app.py
@@ -1,8 +1,7 @@
+# EJERCICIO 5 - previous and next
 # Complete the function to return the previous and next number of a given number
 def previous_next(num):
   # Your code here
-  return None
-
-
+  return num-1, num+1
 # Invoke the function with any integer as its argument
 print(previous_next(179))
