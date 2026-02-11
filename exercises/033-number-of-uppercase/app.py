313d733d538bf7eecca76e41664e43ed879a1125
Alessandra Mantellini <mantellinialessandra13@gmail.com>
2026-02-11 14:27:48 +0100

    Number of uppercase

--- exercises/033-number-of-uppercase/app.py
+++ exercises/033-number-of-uppercase/app.py
@@ -1,1 +1,14 @@
+# EJERCICIO 33 - number of uppercase - no test
 # Your code here
+def number_of_uppercase(letters):
+    counts = {
+    "UPPERCASE": 0,
+    "LOWERCASE": 0
+}
+    for character in letters:
+        if character.isupper():
+            counts["UPPERCASE"] += 1
+        elif character.islower():
+            counts["LOWERCASE"] += 1
+    return "UPPERCASE" + " " + str(counts["UPPERCASE"]) + " " + "LOWERCASE" + " " + str(counts["LOWERCASE"])
+print(number_of_uppercase("Hello world!"))
