65ecde53f22cd37429ffe088f31d64d16bae631c
Alessandra Mantellini <mantellinialessandra13@gmail.com>
2026-02-11 14:21:51 +0100

    Print formula

--- exercises/025-print-formula/app.py
+++ exercises/025-print-formula/app.py
@@ -1,1 +1,11 @@
+# EJERCICIO 25 - print formula
 # Your code here
+import math
+def print_formula(d):
+    c = 50
+    h = 30
+    Q = (2 * c * d) / h
+    root = math.sqrt(Q)
+    result = round(root)
+    return result
+print(print_formula(150))
