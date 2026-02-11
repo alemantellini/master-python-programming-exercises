f46e7c2aefec7ab6e16d11e198eef16d4561e9ad
Alessandra Mantellini <mantellinialessandra13@gmail.com>
2026-02-11 14:26:14 +0100

    Divisable binary

--- exercises/030-divisable-binary/app.py
+++ exercises/030-divisable-binary/app.py
@@ -1,1 +1,13 @@
+# EJERCICIO 30 - divisable binary
 # Your code here
+def divisible_binary(num):
+    my_list = num.split(",")
+    my_new_list = []
+    for i in my_list:
+        decimal = int(i, 2)
+        divisible = decimal % 5 == 0
+        if divisible:
+            my_new_list.append(i)
+    result = ",".join(my_new_list)
+    return result 
+print(divisible_binary("0100,0011,1010,1001"))
