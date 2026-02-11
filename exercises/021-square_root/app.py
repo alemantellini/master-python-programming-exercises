c7a1076139ba34d77563c5ef5bb458d787987fbd
Alessandra Mantellini <mantellinialessandra13@gmail.com>
2026-02-11 14:20:22 +0100

    Square root

--- exercises/021-square_root/app.py
+++ exercises/021-square_root/app.py
@@ -1,1 +1,9 @@
+# EJERCICIO 21 - square root
 # Your code here
+import math
+def square_root(n):
+    root = math.sqrt(n)
+    result= round(root, 2)
+    return result
+print(square_root(50))
+print(square_root(16))
