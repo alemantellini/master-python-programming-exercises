6feaa03ba2702b2694a19a208b9d66c6973ec191
Alessandra Mantellini <mantellinialessandra13@gmail.com>
2026-02-11 13:56:10 +0100

    Swap digits

--- exercises/010-swap_digits/app.py
+++ exercises/010-swap_digits/app.py
@@ -1,7 +1,8 @@
+# EJERCICIO 10 - swap digits
 # Complete the function to return the swapped digits of a given two-digit integer
 def swap_digits(num):
   # Your code here
-  return None
-   
+  s = str(num)
+  return int(s[1] + s[0])
 # Invoke the function with any two-digit integer as its argument
 print(swap_digits(30))
