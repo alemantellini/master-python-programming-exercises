b491a2b0ff1f9ddad1718219635d47d22358c8a0
Alessandra Mantellini <mantellinialessandra13@gmail.com>
2026-02-11 14:32:04 +0100

    Frequency of words

--- exercises/041-frequency-of-words/app.py
+++ exercises/041-frequency-of-words/app.py
@@ -1,1 +1,15 @@
+# EJERCICIO 41 - frequency of words
 # Your code here
+def compute_word_frequency(text):
+    words = text.split()
+    frequency = {}
+    result = ""
+    for word in words:
+        if word in frequency:
+            frequency[word] += 1
+        else:
+            frequency[word] = 1
+    for word in sorted(frequency):
+        result += f"{word}: {frequency[word]}\n"
+    return result[:-1]
+print(compute_word_frequency("New to Python or choosing between Python 2 and Python 3? Read Python 2 or Python 3."))
