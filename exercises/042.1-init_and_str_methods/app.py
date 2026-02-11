b5ece3d8c042d2fbdf0492859188ad27ae0a7968
Alessandra Mantellini <mantellinialessandra13@gmail.com>
2026-02-11 14:33:02 +0100

    Init and str methods

--- exercises/042.1-init_and_str_methods/app.py
+++ exercises/042.1-init_and_str_methods/app.py
@@ -1,2 +1,16 @@
+# EJERCICIO 42.1 - init and str methods
 # Your code here
+class Book:
+    def __init__(self, title, author, year):
+        self.title = title
+        self.author = author
+        self.year = year
 
+    def __str__(self):
+        return f"Book Title: {self.title}\nAuthor: {self.author}\nYear: {self.year}"
+
+# Create an instance of the Book class
+book1 = Book("The Great Gatsby", "F. Scott Fitzgerald", 1925)
+
+# Print the information using the __str__ method
+print(book1)
