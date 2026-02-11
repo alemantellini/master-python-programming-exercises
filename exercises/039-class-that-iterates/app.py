d7a813ce63a2d120f5b308572e47cd43032bda35
Alessandra Mantellini <mantellinialessandra13@gmail.com>
2026-02-11 14:30:54 +0100

    Class that iterates

--- exercises/039-class-that-iterates/app.py
+++ exercises/039-class-that-iterates/app.py
@@ -1,1 +1,13 @@
+# EJERCICIO 39 - class that iterates - no test
 # Your code here
+class DivisibleBySeven:
+    def __init__(self, n):
+        self.n = n
+    def generator(self):
+        for i in range(self.n + 1):
+            if i % 7 == 0:
+                yield i
+n = 50
+div7 = DivisibleBySeven(n)
+for num in div7.generator():
+    print(num)
