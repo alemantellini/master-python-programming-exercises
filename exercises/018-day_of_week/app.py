a3572b4dfb4aa05f9cfe40cec3d3e4b4ee3213d6
Alessandra Mantellini <mantellinialessandra13@gmail.com>
2026-02-11 14:16:32 +0100

    Day of week

--- exercises/018-day_of_week/app.py
+++ exercises/018-day_of_week/app.py
@@ -1,7 +1,15 @@
+# EJERCICIO 18 - day of week
 # Complete the function to return the number of day of the week for k'th day of year
 def day_of_week(k):
-  return None
-
-
+  # 0 = Sunday (day 4)
+  # 1 = Monday (day 5)
+  # 2 = Tuesday (day 6)
+  # 3 = Wednesday (day 7)
+  # 4 = Thursday (day 1)
+  # 5 = Friday (day 2)
+  # 6 = Saturday (day 3)
+  adjustment = (k + 3)
+  result = adjustment % 7
+  return result
 # Invoke function day_of_week with an integer between 1 and 365
-print(day_of_week())
+print(day_of_week(1))
