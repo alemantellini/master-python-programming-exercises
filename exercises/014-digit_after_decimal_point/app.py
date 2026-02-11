f7e4c724877b13235a1e40758a2622b671d8be9e
Alessandra Mantellini <mantellinialessandra13@gmail.com>
2026-02-11 14:08:51 +0100

    Digit after decimal point

--- exercises/014-digit_after_decimal_point/app.py
+++ exercises/014-digit_after_decimal_point/app.py
@@ -1,7 +1,7 @@
+# EJERCICIO 14 - digit after decimal point
 # Complete the function to return the first digit to the right of the decimal point
 def first_digit(num):
-  return None
-
-
+  return int((num % 1) * 10)
 # Invoke the function with a positive real number. ex. 34.33
-print(first_digit())
+print(first_digit(1.79))
+print(first_digit(3.14))
