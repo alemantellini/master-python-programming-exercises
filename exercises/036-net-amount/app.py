f7634bbdcfd8ca0836f00d76368b84c79c719f86
Alessandra Mantellini <mantellinialessandra13@gmail.com>
2026-02-11 14:29:23 +0100

    Net amount

--- exercises/036-net-amount/app.py
+++ exercises/036-net-amount/app.py
@@ -1,1 +1,14 @@
+# EJERCICIO 36 - net amount
 # Your code here
+def net_amount(amount):
+    balance = 0
+    money = amount.split()
+    for i in range(0, len(money), 2):
+        operation = money[i]
+        value = int(money[i + 1])
+        if operation == "D":
+            balance += value
+        elif operation == "W":
+            balance -= value
+    return balance
+print(net_amount("D 300 D 300 W 200 D 100"))
