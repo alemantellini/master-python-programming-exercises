832ce0fbb092c7642d287a300874b1790681dc25
Alessandra Mantellini <mantellinialessandra13@gmail.com>
2026-02-11 14:05:28 +0100

    Last two digits

--- exercises/011-last_two_digits/app.py
+++ exercises/011-last_two_digits/app.py
@@ -1,6 +1,6 @@
+# EJERCICIO 11 - last two digits
 # Complete the function to print the last two digits of an integer greater than 9
 def last_two_digits(num):
-    return None
-
+    return num % 100
 # Invoke the function with any integer greater than 9
-print(last_two_digits())
+print(last_two_digits(1234))
