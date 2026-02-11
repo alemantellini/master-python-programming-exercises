29343b7ae010d8ee35dc573216901d93250a4eb5
Alessandra Mantellini <mantellinialessandra13@gmail.com>
2026-02-11 14:15:23 +0100

    Car route

--- exercises/015-car_route/app.py
+++ exercises/015-car_route/app.py
@@ -1,9 +1,9 @@
-# EJERCICIO 16 - century
-# Complete the function to return the respective number of the century
-def century(year):
+# EJERCICIO 15 - car route
+# Complete the function to return the amount of days it will take to cover a route
+def car_route(n,m):
   import math 
-  number = year / 100
-  result = math.ceil(number)
+  distance = m / n
+  result = math.ceil(distance)
   return result
-# Invoke the function with any given year
-print(century(2001))
+# Invoke the function with two integers
+print(car_route(20, 40))
