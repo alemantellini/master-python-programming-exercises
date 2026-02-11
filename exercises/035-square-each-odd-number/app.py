af78656cc79c716560437d121c01a106c6ba4c18
Alessandra Mantellini <mantellinialessandra13@gmail.com>
2026-02-11 14:28:54 +0100

    Square each odd number

--- exercises/035-square-each-odd-number/app.py
+++ exercises/035-square-each-odd-number/app.py
@@ -1,1 +1,13 @@
+# EJERCICIO 35 - square each odd number - no test
 # Your code here
+def square_odd_numbers(num):
+    my_string = num.split(",")
+    my_new_list = []
+    for i in my_string:
+        n = int(i)
+        odd = n % 2 != 0
+        if odd:
+            square = n**2
+            my_new_list.append(square)
+    return my_new_list
+print(square_odd_numbers("1,2,3,4,5,6,7,8,9"))
