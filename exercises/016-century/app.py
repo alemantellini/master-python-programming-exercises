7e112d745b8f9c209a061fc51798824c77af342c
Alessandra Mantellini <mantellinialessandra13@gmail.com>
2026-02-11 14:15:44 +0100

    Century

--- exercises/016-century/app.py
+++ exercises/016-century/app.py
@@ -1,7 +1,9 @@
+# EJERCICIO 16 - century
 # Complete the function to return the respective number of the century
 def century(year):
-  return None
-
-
+  import math 
+  number = year / 100
+  result = math.ceil(number)
+  return result
 # Invoke the function with any given year
-print(century())
+print(century(2001))
