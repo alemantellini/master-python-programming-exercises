725793c69ca228dbc3363ca0aa81f6e75deaccf6
Alessandra Mantellini <mantellinialessandra13@gmail.com>
2026-02-11 14:26:58 +0100

    Sum eighth digit

--- exercises/031-sum-eigth-digit/app.py
+++ exercises/031-sum-eigth-digit/app.py
@@ -1,1 +1,15 @@
+# EJERCICIO 31 - sum eighth digit (all digits even) - no test
 # Your code here
+def all_digits_even():
+    result = []
+    my_list = range(1000, 3001)
+    for num in my_list:
+        all_even = True
+        for digit in str(num):
+            if int(digit) % 2 != 0:
+                all_even = False
+                break
+        if all_even:
+             result.append(str(num))
+    print(",".join(result))
+all_digits_even()
