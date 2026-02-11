df3b8a5dd0e353b620de67f63dee8745f7d75039
Alessandra Mantellini <mantellinialessandra13@gmail.com>
2026-02-11 14:05:58 +0100

    Tens digit

--- exercises/012-tens_digit/app.py
+++ exercises/012-tens_digit/app.py
@@ -1,7 +1,7 @@
+# EJERCICIO 12 - tens digit
 # Complete the function to return the tens digit of a given integer
 def tens_digit(num):
-  return None
-
-
+  return (num % 100) // 10
 # Invoke the function with any integer
-print(tens_digit())
+print(tens_digit(1234))
+print(tens_digit(179))
